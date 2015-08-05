af04a3f1ac4777c0662f7430f88d08e658ee4055
Martin Robinson <martin.robinson@maths.ox.ac.uk>
2015-08-05 14:46:38 +0100

    added alternate emails to mutt

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd $HOME
-rm .vimrc .tmux.conf
+rm .vimrc .tmux.conf .muttrc
 ln -s $HOME/vimrc/.vimrc .vimrc
 ln -s $HOME/vimrc/.tmux.conf .tmux.conf
+ln -s $HOME/vimrc/.muttrc .muttrc
