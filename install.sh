463a8f1b847152e0943b2cae97ae1ceddad50b01
Martin Robinson <martinjrobins@gmail.com>
2015-12-18 22:37:29 +0000

    install solarized 16 colors for mutt

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ rm .vimrc .tmux.conf .muttrc
 ln -s $HOME/vimrc/.vimrc .vimrc
 ln -s $HOME/vimrc/.tmux.conf .tmux.conf
 ln -s $HOME/vimrc/.muttrc .muttrc
-ln -s $HOME/vimrc/mutt-colors-solarized-dark-256.muttrc mutt-colors-solarized-dark-256.muttrc
+ln -s $HOME/vimrc/mutt-colors-solarized/mutt-colors-solarized-dark-16.muttrc mutt-colors-solarized-dark-16.muttrc
 ln -s $HOME/vimrc/.Xresources .Xresources
